579235642ca7dd516bb47ee66e09b60ebf3f06fb
Oscar Diaz <o-pipe@hotmail.com>
2017-07-29 17:03:33 -0500

    buscandor reto 11 rails plataforma ok :)

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,7 +1,8 @@
 class ExpensesController < ApplicationController
   def index
     @expenses = if params[:concept] && params[:category_id]
-      Expense.where('concept LIKE ?',"%#{params[:concept]}%").where('category_id LIKE ?',"%#{params[:category_id]}%")
+      Expense.where('concept LIKE ?',"%#{params[:concept]}%").
+      where('category_id LIKE ?',"%#{params[:category_id]}%")
     else
         @expenses = Expense.order("date DESC")
     end
